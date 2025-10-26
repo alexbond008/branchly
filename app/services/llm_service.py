c4fac7b8cd236c144b998e39802d40efe0ae241f
alexbond008 <alexbond008@users.noreply.github.com>
2025-10-26 13:30:46 +0100

    create a simple prompt

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -3,6 +3,46 @@ from anthropic import Anthropic
 from dotenv import load_dotenv
 
 
+def generate_assets(transcript :str) -> str:
+    
+    load_dotenv() 
+
+    client = Anthropic(
+        api_key=os.environ.get("CLAUDE_API_KEY")
+    )
+
+    prompt = f"""
+    You are a expert content strategist. Given the transcript of the youtube video:
+    ----
+    {transcript}
+    ----
+    Generate:
+    1. 3 YouTube title suggestions
+    2. 3 timestamps of interesting moments
+    3. A LinkedIn post
+    4. A Twitter post
+
+    Provide the response in the following JSON format:
+    {{
+        "title_suggestions": [list of 3 youtube worthy titles],
+        "timestamps": [list of timestamps],
+        "linkedin_post": "string",
+        "twitter_post": "string"
+    }}
+
+    When generating titles, make them catchy and engaging.
+    For timestamps, pick moments that would intrigue viewers to watch the video.
+    Ensure the LinkedIn and Twitter posts are tailored to their respective audiences and platforms.
+    """
+    reponse = client.messages.create(
+        model="claude-3-haiku-20240307",
+        max_tokens=2000,
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    return reponse.content[0].text
 
 
 
